40877adb1122f5297bd722292cfbfda3703a2f0e
Hendri Pretorius <pretorh@gmail.com>
2016-04-16 15:53:23 +0200

    label util function to resolve label by name
    if 2nd argument specified in cli, use it as the name of a label to find

--- cli/labels.js
+++ cli/labels.js
@@ -4,4 +4,9 @@ var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 var labels = gmail.auth('client_secret.json', SCOPES, 'credentials.json')
 .then(gmail.labels);
 
+if (process.argv[2] !== undefined) {
+    labels = labels
+    .then(gmail.labels.resolveByName(process.argv[2]));
+}
+
 labels.then(console.log).catch(console.error);
