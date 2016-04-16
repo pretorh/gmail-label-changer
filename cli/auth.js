08be776020b0816809d943ffbf08d7f046681cc9
Hendri Pretorius <pretorh@gmail.com>
2016-04-16 15:24:36 +0200

    return promises in auth
    export promises for auth functions

--- cli/auth.js
+++ cli/auth.js
@@ -4,18 +4,19 @@ var fs = require('fs');
 var content = fs.readFileSync('./client_secret.json');
 var secret = JSON.parse(content);
 
+var result;
+
 if (process.argv[2] === 'auth') {
-    auth(secret, [process.argv[3]], 'credentials.json', done);
+    result = auth(secret, [process.argv[3]], 'credentials.json');
 } else if (process.argv[2] === 'save') {
-    auth.finalizeAuth(secret, process.argv[3], 'credentials.json', done);
+    result = auth.finalizeAuth(secret, process.argv[3], 'credentials.json');
 } else {
     console.error('unknown command', process.argv[2]);
 }
 
-function done(err) {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('auth successfull!');
-    }
-}
+result
+.then(function() {
+    console.log('auth successfull!');
+})
+.catch(console.error)
+.done();
