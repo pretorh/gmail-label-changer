7e08cd1cf6719603e320da9f8fe3f0108039fb0d
Hendri Pretorius <pretorh@gmail.com>
2016-04-16 15:32:52 +0200

    move reading of client secret into auth

--- cli/auth.js
+++ cli/auth.js
@@ -1,15 +1,10 @@
 var auth = require('../lib/auth');
-var fs = require('fs');
-
-var content = fs.readFileSync('./client_secret.json');
-var secret = JSON.parse(content);
 
 var result;
-
 if (process.argv[2] === 'auth') {
-    result = auth(secret, [process.argv[3]], 'credentials.json');
+    result = auth('client_secret.json', [process.argv[3]], 'credentials.json');
 } else if (process.argv[2] === 'save') {
-    result = auth.finalizeAuth(secret, process.argv[3], 'credentials.json');
+    result = auth.finalizeAuth('client_secret.json', process.argv[3], 'credentials.json');
 } else {
     console.error('unknown command', process.argv[2]);
 }
