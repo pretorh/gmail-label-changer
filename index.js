01067d5ec745cbb24dc8f175a8a7701e9836590b
Hendri Pretorius <pretorh@gmail.com>
2016-10-24 19:54:25 +0200

    add compose library file to handle sending of messages

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.auth = require('./lib/auth');
 module.exports.labels = require('./lib/labels');
 module.exports.threads = require('./lib/threads');
+module.exports.compose = require('./lib/compose');
